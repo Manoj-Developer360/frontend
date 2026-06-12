b30ab8dc03ebad283795f343324cc38f99660322
Manoj45code <manobala45678@gmail.com>
2026-06-12 13:03:40 +0530

    learn for loop concept.

---
+++ JavaScript/ex5.js
@@ -0,0 +1,35 @@
+// loop
+// for loop
+
+for (count =1;count <=10;count=count+1){
+    console.log("Manoj")
+}
+
+// print all the numbers from 1 to 10 using for loop
+for (count =1;count<=10;count=count+1){
+   console.log(count)
+}
+
+ // reverse
+for (count =10;count>=1;count=count-1){
+    console.log(count)
+}
+
+// print numbers from 1 to 10, but increment by 2 in each step using for loop
+
+for (count=1;count<=10;count=count+2){
+    console.log(count)
+}
+
+// print only the even numbers from 1 to 10
+for  (count =1;count<=10;count=count+1){
+    if(count %2 == 0){
+        console.log(count)
+    }
+}
+
+// print 2 table
+
+for (count =1;count<=10;count=count+1){
+   console.log(count+"*2 =",count*2)
+}
