8d83ca7d4d8eeb89e51d8e523891709559c5e6e2
Manoj45code <manobala45678@gmail.com>
2026-06-12 09:52:56 +0530

    Learn and Practice Function concepts.

---
+++ JavaScript/ex3.js
@@ -0,0 +1,57 @@
+// JavaScript Functions
+
+function hi ()
+{
+    console.log("Function is Working")
+}
+hi()
+
+function Vivo ()
+{
+    console.log("This is Vivo Mobile")
+}
+
+function MyName (){
+    console.log("My Name is Manoj Kumar V")
+}
+
+Vivo()
+MyName()
+
+//Exercise
+
+// - Create two variables a and b
+// - Create a function called add
+// This Function should print addition of two Numbers
+
+var a = 10
+var b = 20
+function Add ()
+{
+    console.log(a+b)
+}
+Add()
+
+
+
+// Create a variable called factor, fplayer, fmovie
+// Create a function called favourite()
+// Everytime you call this function, this function should return the following output
+
+// O/P
+// Favourite Actor : Vijay
+// Favourite Player : Virat
+// Favourite Movie : Vettaikaaran
+
+
+var factor = "Vijay"
+var fplayer = "Virat"
+var fmovie = "Vettaikaaran"
+
+function favourite () {
+    console.log("Favourite Actor : "+ factor)
+    console.log("Favourite Player : "+ fplayer)
+    console.log("Favourite Movie : "+ fmovie)
+}
+
+favourite()
